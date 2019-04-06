e2c887c6eeeee2d5edd705f9f9d67816d1d62f5c
nift4 <36458596+nift4@users.noreply.github.com>
2019-04-06 13:50:43 +0200

    Commented code

--- app/src/main/java/org/nift4/btdemo/MainActivity.java
+++ app/src/main/java/org/nift4/btdemo/MainActivity.java
@@ -30,9 +30,11 @@ import android.content.DialogInterface;
 
 public class MainActivity extends Activity 
 {
+	// Bluetooth Service
 	BluetoothService mBluetoothService;
+	// Was connected in this session
 	private boolean wasConnected = false;
-
+	// UUIDs
 	private String insecUuid = "071cb722-6728-4fb4-92a1-1ec0bcbee21e";
 	private String secUuid = "53fcc9ce-6227-470b-935f-248efc3b3968";
 	
@@ -41,12 +43,16 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		// Create handlers to use h.postDelay()
 		final Handler h = new Handler(this.getMainLooper());
 		try {
+			// Create Bluetooth Service
 			mBluetoothService = new BluetoothService(this, UUID.fromString(insecUuid), UUID.fromString(secUuid), new BluetoothService.Callback(){
+				// Bluetooth Enabled Callback
 					@Override
 					public void callback()
 					{
+						// Add functions to the butzons
 						((Button) findViewById(R.id.server_btn)).setOnClickListener(new View.OnClickListener(){
 								@Override
 								public void onClick(View p1)
@@ -65,6 +71,9 @@ public class MainActivity extends Activity
 								@Override
 								public void onClick(View p1)
 								{
+									// Get BtData object
+									// buffer is byte[]
+									// bytes is count of the usable data bytes in buffer
 									BluetoothService.BtData d = mBluetoothService.read();
 									if (d != null) {
 										new AlertDialog.Builder(MainActivity.this)
@@ -74,11 +83,13 @@ public class MainActivity extends Activity
 									}
 								}
 							});
+						// Wait 500ms
 						h.postDelayed(new Runnable(){
 							@Override
 							public void run()
 							{
 								boolean nodelay = false;
+								// If state is connected for the first time
 								if (mBluetoothService.getState() == mBluetoothService.STATE_CONNECTED && !wasConnected) {
 									wasConnected = true;
 									new AlertDialog.Builder(MainActivity.this)
@@ -93,6 +104,7 @@ public class MainActivity extends Activity
 										. setMessage("Connected")
 										. setTitle("Bluetooth")
 										. show();
+								// If connection lost
 								} else if (mBluetoothService.getState() == mBluetoothService.STATE_NONE && mBluetoothService.getNoneReason() == mBluetoothService.NONE_REASON_CONN_LOST) {
 									new AlertDialog.Builder(MainActivity.this)
 									. setCancelable(false)
@@ -107,6 +119,7 @@ public class MainActivity extends Activity
 									. setMessage("Connection lost")
 									. setTitle("Bluetooth Error")
 									. show();
+									// "break;" this loop
 									nodelay = true;
 								}
 								if(!nodelay){
@@ -116,6 +129,7 @@ public class MainActivity extends Activity
 						},500);
 					}
 				}, new BluetoothService.Callback(){
+					// Enable failed
 					@Override
 					public void callback()
 					{
@@ -131,18 +145,26 @@ public class MainActivity extends Activity
 		}
     }
 	private void onClientStart(){
+		// View device selector
 		mBluetoothService.showDeviceSelector();
+		// See onActivityResult()
 	}
 	private void onServerStart() {
+		// Make discoverable
 		mBluetoothService.makeDiscoverable();
+		// Start listener
 		mBluetoothService.startServer();
+		// Create time helper
 		final Handler h = new Handler(this.getMainLooper());
+		// Run after 1000ms
 		h.postDelayed(new Runnable(){
 				@Override
 				public void run()
 				{
+					// If connected
 					if (mBluetoothService.getState() == mBluetoothService.STATE_CONNECTED) {
 						mBluetoothService.write("Hello from Server!".getBytes());
+						// Send other message after 1s
 						new Handler(MainActivity.this.getMainLooper()).postDelayed(new Runnable(){
 								@Override
 								public void run()
@@ -150,6 +172,7 @@ public class MainActivity extends Activity
 									mBluetoothService.write("hellohl222".getBytes());
 								}
 							},1000);
+						// Else wait 1s and check again
 					} else {
 						h.postDelayed(this,1000);
 					}
@@ -157,15 +180,23 @@ public class MainActivity extends Activity
 		},1000);
 	}
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// Get device selector result
+		// IMPORTANT: Add this call always!! It's needed to enable Bluetooth
 		BluetoothDevice device = mBluetoothService.getDeviceSelectorResult(requestCode,resultCode,data);
+		// If Device Selector returned
 		if (device != null) {
+			// Create another handler
 			final Handler h = new Handler(this.getMainLooper());
+			// Try connection
 			mBluetoothService.connect(device,true);
+			// Run after delay
 			h.postDelayed(new Runnable(){
 					@Override
 					public void run()
 					{
+						// If connected run this
 						if (mBluetoothService.getState() == mBluetoothService.STATE_CONNECTED) {
+							// View connected dialog
 							new AlertDialog.Builder(MainActivity.this)
 								. setCancelable(true)
 								. setNeutralButton("Ok", new DialogInterface.OnClickListener(){
@@ -178,10 +209,12 @@ public class MainActivity extends Activity
 								. setMessage("Connected")
 								. setTitle("Bluetooth")
 								. show();
+							// Run after delay (500ms)
 							h.postDelayed(new Runnable() {
 								@Override
 								public void run()
 								{
+									// If connection lost
 									if (mBluetoothService.getState() == mBluetoothService.STATE_NONE && mBluetoothService.getNoneReason() == mBluetoothService.NONE_REASON_CONN_LOST) {
 										new AlertDialog.Builder(MainActivity.this)
 											. setCancelable(false)
@@ -196,8 +229,11 @@ public class MainActivity extends Activity
 											. setMessage("Connection lost")
 											. setTitle("Bluetooth Error")
 											. show();
+									// If new message
 									} else if (mBluetoothService.getState() == mBluetoothService.STATE_CONNECTED && mBluetoothService.newMessageAvailable()) {
+										// Send text
 										mBluetoothService.write("Hello back from Client!".getBytes());
+										// Send more data afetr delay
 										h.postDelayed(new Runnable(){
 												@Override
 												public void run()
@@ -205,11 +241,13 @@ public class MainActivity extends Activity
 													mBluetoothService.write("all2".getBytes());
 												}
 											}, 1000);
+										// If not connection lost or new message, run this if in 500ms again
 									} else {
 										h.postDelayed(this,500);
 									}
 								}
 							}, 500);
+						// If connection failed
 						} else if (mBluetoothService.getState() == mBluetoothService.STATE_NONE && mBluetoothService.getNoneReason() == mBluetoothService.NONE_REASON_CONN_FAIL) {
 							new AlertDialog.Builder(MainActivity.this)
 								. setCancelable(false)
@@ -224,12 +262,14 @@ public class MainActivity extends Activity
 								. setMessage("Connection failed")
 								. setTitle("Bluetooth Error")
 								. show();
-							
+						// Else run this 1000 ms later to wait for connection result
 						} else {
 							h.postDelayed(this,1000);
 						}
 					}
 			},1000);
+		} else {
+			// Other activityResult
 		}
 	}
 }
